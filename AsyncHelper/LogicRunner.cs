14ae253c18127efca7fc729121467ebe31d40280
Roger Pence <roger.pence@gmail.com>
2021-11-22 16:32:38 -0600

    Initial commit

---
+++ AsyncHelper/LogicRunner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncHelper
+{
+    public class LogicRunner
+    {
+        AVRLogic.LogicFunctions lf = new AVRLogic.LogicFunctions();
+
+        public void Run()
+         {
+            Task.WaitAll(f1(), f2(), f3());
+        }
+
+        async Task f1()
+        {
+            await Task.Run(() => lf.F1("async"));
+        }
+
+        async Task f2()
+        {
+            await Task.Run(() => lf.F2("async"));
+        }
+
+        async Task f3()
+        {
+            await Task.Run(() => lf.F3("async"));
+        }
+
+
+    }
+}
